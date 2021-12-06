d54b7b4085878edd5e2d8e925e2760feb9a290f5
Rahul Chopra <scorpio.sk98@gmail.com>
2021-12-06 13:13:23 +0530

    Expense Comoponent

---
+++ components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Items</h2>
+      <ul>
+        <li>Food Rs10</li>
+        <li>Petrol Rs100</li>
+        <li>Movie Rs200</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ExpenseItem;
