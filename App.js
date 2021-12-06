352a2a6f891a4316d5170bcb55551dc7e202ef9a
Rahul Chopra <scorpio.sk98@gmail.com>
2021-12-06 22:40:51 +0530

    Loop for Expenses

---
+++ App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      LocationOfExpenditure: 'Italy',
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, LocationOfExpenditure: 'Italy', date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      LocationOfExpenditure: 'Paris',
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      LocationOfExpenditure: 'London',
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  return (
+    <div>
+      {expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          LocationOfExpenditure={expense.LocationOfExpenditure}
+          date={expense.date}
+        ></ExpenseItem>
+      ))}
+    </div>
+  );
+}
+
+export default App;
